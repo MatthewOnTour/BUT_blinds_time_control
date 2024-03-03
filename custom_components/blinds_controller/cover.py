1310916d04f0e6aab7cbe6b89496dd226a1f27a1
Matúš <m.justik@hotmail.com>
2024-03-03 16:37:47 +0100

    test the controls

--- custom_components/blinds_controller/cover.py
+++ custom_components/blinds_controller/cover.py
@@ -1,29 +1,22 @@
-from homeassistant.components.cover import CoverEntity
-from .const import DOMAIN
-
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    # Get the blinds data from the config entry
-    blinds_data = hass.data[DOMAIN][config_entry.entry_id]
-
-    # Create a list of BlindsCover entities
-    entities = [BlindsCover(blinds_data)]
-
-    # Add the entities
-    async_add_entities(entities, True)
+from homeassistant.components.switch import is_on, turn_on, turn_off
 
 class BlindsCover(CoverEntity):
-    def __init__(self, blinds_data):
+    def __init__(self, hass, blinds_data):
+        self.hass = hass
         self.blinds_data = blinds_data
 
     @property
     def name(self):
         return self.blinds_data["ent_name"]
 
-    @property
-    def is_closed(self):
-        # Replace this with the actual logic to determine if the cover is closed
-        return self.blinds_data.get("is_closed", None)
-
     def open_cover(self, **kwargs):
-        # Replace this with the actual logic to open the cover
-        self.blinds_data["is_closed"] = False
+        # Call the service of the up button entity
+        up_button_entity_id = self.blinds_data["entity_up"]
+        if not is_on(self.hass, up_button_entity_id):
+            turn_on(self.hass, up_button_entity_id)
+
+    def close_cover(self, **kwargs):
+        # Call the service of the down button entity
+        down_button_entity_id = self.blinds_data["entity_down"]
+        if not is_on(self.hass, down_button_entity_id):
+            turn_on(self.hass, down_button_entity_id)
