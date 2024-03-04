7b9eb1b8c3fc70774dc283ad7c4d83a299240667
Matúš <m.justik@hotmail.com>
2024-03-04 20:04:47 +0100

    conection with device succesful

--- custom_components/blinds_controller/cover.py
+++ custom_components/blinds_controller/cover.py
@@ -1,6 +1,7 @@
 from homeassistant.components.cover import CoverEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.event import async_call_later
 
 from .const import DOMAIN
 
@@ -11,6 +12,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
 class BlindsCover(CoverEntity):
     def __init__(self, entry: ConfigEntry):
         self.entry = entry
+        self._state = None  # Initialize _state attribute
 
     @property
     def unique_id(self):
@@ -23,7 +25,55 @@ class BlindsCover(CoverEntity):
         return self.entry.data["ent_name"]
 
     @property
+    def extra_state_attributes(self):
+        """Return the device state attributes."""
+        return {
+            "entity_up": self.entry.data["entity_up"],
+            "entity_down": self.entry.data["entity_down"],
+            "time_up": self.entry.data["time_up"],
+            "time_down": self.entry.data["time_down"],
+            "tilt_open": self.entry.data["tilt_open"],
+            "tilt_closed": self.entry.data["tilt_closed"],
+        }
+
+    @property
     def is_closed(self):
-        # This is a required property, but since this entity doesn't actually control a device,
-        # we'll just return None to indicate that the closed state is unknown
-        return None
+        if self._state is None:
+            return None
+        return not self._state
+
+    async def _async_handle_command(self, command):
+        if command == 'open_cover':
+            await self.hass.services.async_call('homeassistant', 'turn_on', {
+                'entity_id': self.entry.data["entity_up"],
+            }, False)
+            self._state = True
+
+        elif command == 'close_cover':
+            await self.hass.services.async_call('homeassistant', 'turn_on', {
+                'entity_id': self.entry.data["entity_down"],
+            }, False)
+            self._state = False
+
+        elif command == 'stop_cover':
+            await self.hass.services.async_call('homeassistant', 'turn_off', {
+                'entity_id': self.entry.data["entity_up"],
+            }, False)
+            await self.hass.services.async_call('homeassistant', 'turn_off', {
+                'entity_id': self.entry.data["entity_down"],
+            }, False)
+            self._state = None
+
+        # Update state of entity
+        self.async_write_ha_state()
+
+    async def async_open_cover(self, **kwargs):
+        await self._async_handle_command('open_cover')
+
+    async def async_close_cover(self, **kwargs):
+        await self._async_handle_command('close_cover')
+
+    async def async_stop_cover(self, **kwargs):
+        await self._async_handle_command('stop_cover')
+
+    
